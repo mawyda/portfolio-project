3ee96c4e67a4d009e11a74854810eaea5d0d72b1
Matt Dalton <mdalton142@gmail.com>
2018-10-05 09:28:06 -0400

    Added bootstrap and jobs are now showing

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length = 255) 
+	pub_date = models.DateTimeField() # publication date 
+	body = models.TextField() # text of post 
+	image = models.ImageField(upload_to = 'images\\') 
+	 
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Job # import the Jobs model
 # Create your views here.
+
+def home(request):
+	jobs = Job.objects 
+	return render(request, 'jobs\home.html', {'jobs': jobs})
